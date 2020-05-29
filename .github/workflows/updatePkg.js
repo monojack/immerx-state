e6a59bf1418d8a908307fa752c1ff6bdf94e35d4
Ionut Achim <achimvionut@gmail.com>
2020-05-29 13:51:53 +0300

    build: add npm scoped publish job

---
+++ .github/workflows/updatePkg.js
@@ -0,0 +1,7 @@
+const writePackage = require('write-pkg')
+const readPackage = require('read-pkg')
+
+const opts = { normalize: false }
+const json = readPackage.sync(opts)
+
+writePackage({ ...json, name: `@immerx/state` })
